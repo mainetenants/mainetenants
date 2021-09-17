8fd8119e21d90f4bcf98e9834d3c02af4416292b
Heena <chh.odesk.mca@gmail.com>
2021-09-17 18:44:41 +0530

    changes

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,7 +3,6 @@
 
 <title>Winku Social Network Toolkit</title>
 <link rel="icon" href="{{ asset('assets/images/fav.png') }}" type="image/png" sizes="16x16">
-<link rel="stylesheet" href="{{ asset('assets/theme.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/main.min.css')}}">
 <link rel="icon" href="{{ asset('assets/css/favicon.jpg') }}">
 <link rel="stylesheet" href="{{ asset('assets/themify-icons/themify-icons.css')}}">
